6dcaf53d89f7a10597aee438ec9065aedc98fec6
thiagodeas <sousaalvesth@gmail.com>
2025-04-29 10:45:29 -0300

    feat(migration): create outputs table for tracking product outputs

---
+++ database/migrations/2025_04_29_133743_create_outputs_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('outputs', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id', 4);
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->string('reason');
+            $table->date('output_date');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('outputs');
+    }
+};
