34e8f85f98d840d00fbf045cd18e8b0531da0d00
thiagodeas <sousaalvesth@gmail.com>
2025-05-02 21:57:27 -0300

    feat(routes): add routes for entries and outputs with respective controllers

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Entry\EntryController;
+use App\Http\Controllers\Output\OutputController;
+use App\Http\Controllers\Product\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('products')->group(function () {
@@ -9,4 +11,20 @@ Route::prefix('products')->group(function () {
     Route::get('/{id}', [ProductController::class, 'getById']);
     Route::put('/{id}', [ProductController::class, 'update']);
     Route::delete('/{id}', [ProductController::class, 'delete']);
+});
+
+Route::prefix('entries')->group(function () {
+    Route::get('/', [EntryController::class, 'getAll']);
+    Route::post('/', [EntryController::class, 'create']);
+    Route::get('/{id}', [EntryController::class, 'getById']);
+    Route::get('/product/{id}', [EntryController::class, 'getByProductId']);
+    Route::get('/filter', [EntryController::class, 'getByDateRange']);
+});
+
+Route::prefix('outputs')->group(function () {
+    Route::get('/', [OutputController::class, 'getAll']);
+    Route::post('/', [OutputController::class, 'create']);
+    Route::get('/{id}', [OutputController::class, 'getById']);
+    Route::get('/product/{id}', [OutputController::class, 'getByProductId']);
+    Route::get('/filter', [OutputController::class, 'getByDateRange']);
 });
