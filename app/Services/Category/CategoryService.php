9a6cc06fcd0fdb3691b053b04888955f596cc63d
thiagodeas <sousaalvesth@gmail.com>
2025-05-05 10:28:09 -0300

    feat(category): add GetCategoryByNameRequest and CategoryService for category management

---
+++ app/Services/Category/CategoryService.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Services\Category;
+
+use App\Models\Category;
+use App\Repositories\Category\CategoryRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use InvalidArgumentException;
+
+class CategoryService 
+{
+    protected CategoryRepositoryInterface $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function createCategory(array $data): Category
+    {
+        $existingCategory = $this->categoryRepository->getByName($data['name']);
+
+        if($existingCategory) {
+            throw new InvalidArgumentException('The category already exists.');
+        }
+
+        return $this->categoryRepository->create($data);
+    }
+
+    public function getAllCategories(): Collection
+    {
+        return $this->categoryRepository->getAll();
+    }
+
+    public function getCategoryById(string $id): Category
+    {
+        $category = $this->categoryRepository->getById($id);
+
+        if (!$category) {
+            throw new ModelNotFoundException('Category not found');
+        }
+
+        return $category;
+    }
+
+    public function getCategoryByName(string $name): Category
+    {
+        $category = $this->categoryRepository->getByName($name);
+
+        if (!$category) {
+            throw new ModelNotFoundException('Category not found');
+        }
+
+        return $category;
+    }
+
+    public function deleteCategory(string $id): bool
+    {
+        $category = $this->categoryRepository->getById($id);
+
+        if (!$category) {
+            throw new ModelNotFoundException('Category not found.');
+        }
+
+        return $this->categoryRepository->delete($id);
+    }
+}
