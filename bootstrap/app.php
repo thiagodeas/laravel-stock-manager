976b1077fc6c9f15b3ca75905b8bc9ae4b2085a8
thiagodeas <sousaalvesth@gmail.com>
2025-05-07 16:46:51 -0300

    feat(exception): add global exception handler for JSON responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ use App\Exceptions\Product\ProductNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,6 +24,14 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (Throwable $e, Request $request) {
+            $statusCode = $e->getCode() ? : 500;
+        
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], $statusCode);
+        });
+
         $exceptions->renderable(function (CategoryNotFoundException $e, $request) {
             return response()->json([
                 'error'=> $e->getMessage(),
