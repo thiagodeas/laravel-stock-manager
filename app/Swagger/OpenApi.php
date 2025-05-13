eb19706d0578a3c6fd9b6eceac8e897c3e7ec35b
thiagodeas <sousaalvesth@gmail.com>
2025-05-13 08:29:59 -0300

    feat: add OpenAPI configuration and initial API documentation

---
+++ app/Swagger/OpenApi.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\OpenApi(
+ *     openapi="3.0.0",
+ *     @OA\Info(
+ *         title="Laravel Stock Manager API",
+ *         version="1.0.0",
+ *         description="API documentation for the Laravel Stock Manager application."
+ *     ),
+ * )
+ */
+class OpenApi
+{
+    // 
+}
